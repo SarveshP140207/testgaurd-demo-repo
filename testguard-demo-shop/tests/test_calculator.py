6d190b268dcbd5cadda5b0fe84a41c9a8f33dd20
Sarvesh P <sarveshp111169@gmail.com>
2026-09-22 20:33:03 +0530

    fixed calculator error and added timeout test

--- testguard-demo-shop/tests/test_calculator.py
+++ testguard-demo-shop/tests/test_calculator.py
@@ -1,7 +1,7 @@
 from app.calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 99
+    assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 2) == 3
@@ -11,3 +11,7 @@ def test_multiply():
 
 def test_divide():
     assert divide(10, 2) == 5
+
+import time 
+def test_timeout():
+    time.sleep(70)
